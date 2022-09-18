fe36c6364d1b98f9508c4c68c37b7b4ebad9d2d0
김태훈 <bigteach0508@pusan.ac.kr>
2022-09-18 16:13:13 +0900

    javascript_practice

---
+++ javascript_basic2/myfirstJavscript/app.js
@@ -0,0 +1,25 @@
+const a=3;
+let b=2;
+
+console.log(a);
+console.log(b);
+
+const arr = [1,2,3,4,5];
+arr.push(6);
+arr.pop();
+console.log(arr.length)
+//arr.length();
+
+let player ={
+    name:"nick",
+    age:22,
+    points:100,
+}
+
+console.log(player.name);
+player.name = "hello";
+console.log(player.name);
+
+function func(){
+    console.log("Hello!");
+}

7100d68646b8b6bc8afb67944cc3d5d4c2f3f928
김태훈 <bigteach0508@pusan.ac.kr>
2022-09-22 12:56:58 +0900

    javasccript practice

--- javascript_basic2/myfirstJavscript/app.js
+++ javascript_basic2/myfirstJavscript/app.js
@@ -1,3 +1,9 @@
+function func(name){
+    console.log("Hello! " + name);
+    return 1;
+}
+
+
 const a=3;
 let b=2;
 
@@ -10,16 +16,23 @@ arr.pop();
 console.log(arr.length)
 //arr.length();
 
-let player ={
-    name:"nick",
+const player ={
+    name:"Nick",
     age:22,
     points:100,
+    hello:function(yourname){
+        console.log("hello! in object - " + yourname);
+        return yourname;
+    }
 }
 
 console.log(player.name);
 player.name = "hello";
 console.log(player.name);
+yourname = player.hello("Donald");
+console.log(yourname);
+func("James")
 
-function func(){
-    console.log("Hello!");
-}
+//const age = prompt("how old are you? : ");
+
+//console.log(age);

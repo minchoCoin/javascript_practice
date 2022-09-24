0580cd65dc98cab0ed3803d02d8e7bfd5ef8c73e
김태훈 <bigteach0508@pusan.ac.kr>
2022-09-24 10:41:53 +0900

    loginform practice

---
+++ login/app.js
@@ -0,0 +1,10 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);

add459f4123011dd0245ecdff4f64f2497b6e71f
김태훈 <bigteach0508@pusan.ac.kr>
2022-09-24 11:24:11 +0900

    javascript practice

--- login/app.js
+++ login/app.js
@@ -1,5 +1,7 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
+const NaverLink = document.querySelector(".ToNaver a");
+const SimpleNotice = document.querySelector(".SimpleNotice p");
 
 function onLoginSubmit(event){
     event.preventDefault();
@@ -7,4 +9,16 @@ function onLoginSubmit(event){
     console.log(username);
 }
 
+function onNaverClick(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+function onCopyNotice(event){
+    event.preventDefault();
+    event.clipboardData.setData("Text","Do not copy!");
+}
+
 loginForm.addEventListener("submit",onLoginSubmit);
+NaverLink.addEventListener("click", onNaverClick);
+SimpleNotice.addEventListener("copy",onCopyNotice);
